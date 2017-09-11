02742d5d68aff8d292478dadd585812f7309eb71
Colin Chalmers <Colin.Chalmers@simplex-it.nl>
2017-09-11 22:45:09 +0200

    Updated function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,12 +10,25 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:348581565820:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.simplex-it.click',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print 'Building portfolio from location ' + str(location)
         portfolio_bucket = s3.Bucket('portfolio.simplex-it.click')
-        build_bucket = s3.Bucket('portfoliobuild.simplex-it.click')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -27,6 +40,9 @@ def lambda_handler(event, context):
         print 'Job done!'
 
         topic.publish(Subject='Portfolio deployed', Message='Portfolio deployed successfully')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject='Portfolio deploy failed', Message='Portfolio NOT deployed successfully')
         raise
